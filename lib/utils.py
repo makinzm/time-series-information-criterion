ced102afa8f0d2681a3f130ca4c57324ef793e46
makinzm <nozomi.maki.da@gmail.com>
2024-09-12 06:33:12 +0900

    feat: set_sed

--- lib/utils.py
+++ lib/utils.py
@@ -1,1 +1,15 @@
 """シードを整えるなどの、ユーティリティ関数を提供するmodule."""
+
+import numpy as np
+
+def set_seed(seed: int) -> None:
+    """シードを設定する関数.
+
+    Args:
+        seed (int): シードの値.
+
+    Returns:
+        None
+    """
+    np.random.seed(seed)
+
